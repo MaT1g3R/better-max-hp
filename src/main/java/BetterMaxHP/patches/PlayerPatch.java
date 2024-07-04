ed93bdc255241fe7ce0b20f51adb31caf3c6d39b
Peijun Ma <peijun.ma@protonmail.com>
2024-07-04 15:25:57 -0400

    refactor: move reflection methods into class

--- src/main/java/BetterMaxHP/patches/PlayerPatch.java
+++ src/main/java/BetterMaxHP/patches/PlayerPatch.java
@@ -9,76 +9,11 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
+import static BetterMaxHP.Utils.*;
 import static com.megacrit.cardcrawl.characters.AbstractPlayer.PlayerClass.WATCHER;
 
 public class PlayerPatch {
 
-    private static Method findMethod(Class clz, String methodName, Class... parameterTypes)
-            throws NoSuchMethodException {
-        try {
-            return clz.getDeclaredMethod(methodName, parameterTypes);
-        } catch (NoSuchMethodException e) {
-            Class superClass = clz.getSuperclass();
-            if (superClass == null) {
-                throw e;
-            }
-            return findMethod(superClass, methodName, parameterTypes);
-        }
-    }
-
-    private static Field findField(Class clz, String fieldName) throws NoSuchFieldException {
-        try {
-            return clz.getDeclaredField(fieldName);
-        } catch (NoSuchFieldException e) {
-            Class superClass = clz.getSuperclass();
-            if (superClass == null) {
-                throw e;
-            }
-            return findField(superClass, fieldName);
-        }
-    }
-
-    private static Object invokeMethod(Object object, Method method, Object... args)
-            throws InvocationTargetException, IllegalAccessException {
-        method.setAccessible(true);
-        return method.invoke(object, args);
-    }
-
-    private static <A> A getField(Object obj, String name) {
-        Field field;
-        A result;
-        try {
-            field = findField(obj.getClass(), name);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-        field.setAccessible(true);
-        try {
-            result = (A) field.get(obj);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-        return result;
-    }
-
-    private static <A> void setField(Object obj, String name, A value) {
-        Field field;
-        try {
-            field = findField(obj.getClass(), name);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-        field.setAccessible(true);
-        try {
-            field.set(obj, value);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static void reloadAnimation(float ratio) {
         AbstractPlayer player = AbstractDungeon.player;
